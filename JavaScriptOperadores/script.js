439455bedc878a65d6aa3e6e3ec5a5945b1e9079
duduwest <123770322+duduwest@users.noreply.github.com>
2023-05-12 17:38:10 +0000

    lmr

---
+++ JavaScriptOperadores/script.js
@@ -0,0 +1,8 @@
+let nome; 
+let sobrenome; 
+
+nome = prompt ("Digite o seu Nome","Digite aqui"); 
+sobrenome = prompt ("Digite o seu Sobreome","Digite aqui");  
+
+alert ("Óla " +nome+ " "+sobrenome); 
+
